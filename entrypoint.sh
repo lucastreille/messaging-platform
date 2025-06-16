4c08a1a3493b9a92d6e77df7c59832265eed0ba5
Saadct <92342579+Saadct@users.noreply.github.com>
2025-06-16 16:33:31 +0200

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
-echo "Running Prisma generate and migrations..."
+echo "Checking Redis connection..."
 
+node check-redis.js
+if [ $? -ne 0 ]; then
+  echo "Redis connection test failed, exiting."
+  exit 1
+fi
+
+echo "Running Prisma generate and migrations..."
 # npx prisma generate
 # npx prisma migrate deploy --schema=src/prisma/schema.prisma
-
 
 echo "Starting NestJS app..."
 node dist/main.js
