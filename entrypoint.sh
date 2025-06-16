399e5411322703084962af4360a9781dae780392
Saadct <92342579+Saadct@users.noreply.github.com>
2025-06-16 11:04:58 +0200

    fix script sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 
-until nc -z -v -w30 postgres 5432
-do
-  echo "Waiting for Postgres to start..."
-  sleep 1
-done
-
-echo "Postgres is up. Running Prisma generate and migrations..."
+echo "Running Prisma generate and migrations..."
 npx prisma generate
 npx prisma migrate deploy
 
