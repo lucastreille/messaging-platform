e2bfcb4b56409c6c22f3b8a4a5effa145d193370
Saadct <92342579+Saadct@users.noreply.github.com>
2025-06-16 12:50:44 +0200

    fix prisma

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 echo "Running Prisma generate and migrations..."
+
 npx prisma generate
-npx prisma migrate deploy
+npx prisma migrate deploy --schema=src/prisma/schema.prisma
+
 
 echo "Starting NestJS app..."
 node dist/main.js
